01b5b8a3c439e96d6d363e1e1ce49bf30d8b87be
rim112 <rim.thaimi@gmail.com>
2020-05-26 01:52:09 +0000

    2eme Modification de projet Rim

--- src/main/java/org/sid/AuthentitationApplication.java
+++ src/main/java/org/sid/AuthentitationApplication.java
@@ -1,24 +1,20 @@
 package org.sid;
 
 
-import org.sid.dao.TaskRepository;
 import org.sid.entities.AppRole;
 import org.sid.entities.AppUser;
-import org.sid.entities.Task;
 import org.sid.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @SpringBootApplication
 public class AuthentitationApplication implements CommandLineRunner {
-	@Autowired
-	private  TaskRepository taskRepository;
+
 	
 	@Autowired 
 	private AccountService  accountService;
@@ -32,20 +28,19 @@ public class AuthentitationApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... arg0) throws Exception {
-		accountService.saveUser(new AppUser(null,"yassine@mail.com","elhouri","yassin",1980,"Rabat","+212671990829","123",null));
-
+	/*	accountService.saveUser(new AppUser(null,"rim.ben@gmail.com","rim","benboukhourse",1999,"Rabat","+212671990829","abcd123",null));
+/*
 		accountService.saveUser(new AppUser(null,"user@mail.com","nizar","mehdi",1999,"Rabat","+212671990829","1234",null));
 		accountService.saveRole(new AppRole(null,"ADMIN"));
 		accountService.saveRole(new AppRole(null,"USER"));
-		
-		accountService.addRoleToUse("yassine@mail.com","ADMIN");
 		accountService.addRoleToUse("user@mail.com","USER");
+accountService.saveRole(new AppRole(null,"ADMIN"));
+		accountService.addRoleToUse("rim.ben@gmail.com","ADMIN");*/
+		
 
 
 
 
-		taskRepository.save(new Task (null,"T1"));
-		taskRepository.save(new Task (null,"T2"));
 
 
 	}
